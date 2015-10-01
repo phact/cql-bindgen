71cc7811a5f95bd32357ff87aac500bebd893155
Sebastián Estévez <estevezsebastian@gmail.com>
2015-10-01 01:37:33 -0400

    Update build.rs

--- build.rs
+++ build.rs
@@ -8,6 +8,5 @@ fn main() {
     println!("cargo:rustc-flags=-l dylib=ssl");
     println!("cargo:rustc-flags=-l dylib=stdc++");
     println!("cargo:rustc-flags=-l dylib=uv");
-    println!("cargo:rustc-link-search={}", "/usr/local/lib");
     println!("cargo:rustc-link-lib=static=cassandra_static");
 }
